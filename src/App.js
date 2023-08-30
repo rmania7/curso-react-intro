eacfe458ee6e720b7ddef8a046cdbc3ec7847d6e
rmania7 <rmania7@gmail.com>
2023-08-30 10:53:56 -0600

    Se crean los primeros componesntes de React todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+          <TodoCounter/>
+          <TodoSearch/>
+          <TodoList>
+            <TodoItem/>  
+            <TodoItem/> 
+            <TodoItem/> 
+          </TodoList>    
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
     </div>
+  );
+}
+
+function TodoItem(){
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
