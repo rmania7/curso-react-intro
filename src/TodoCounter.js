5941ed5a095dd21153b755a520e7e423ec258fd3
rmania7 <rmania7@gmail.com>
2023-08-30 10:55:07 -0600

    Se crean los primeros componesntes de React todo2

---
+++ src/TodoCounter.js
@@ -0,0 +1,7 @@
+function TodoCounter(){
+    return(
+      <h1>Completados 3 de 5 ToDos</h1>
+    );
+  }
+
+  export {TodoCounter};
---
+++ src/TodoSearch.js
@@ -0,0 +1,7 @@
+function TodoSearch(){
+    return(
+     <input placeholder="pelar cebolla" />
+    );
+}
+
+export {TodoSearch};
